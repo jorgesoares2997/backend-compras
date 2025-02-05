43133facecfe892267ae9251485266f9add35830
jorge soares <jorgesoares@jorges-MacBook-Air.local>
2025-02-05 17:32:07 -0300

    fix:mvn package

--- src/main/java/com/jorge/CrudSpringApplication.java
+++ src/main/java/com/jorge/CrudSpringApplication.java
@@ -1,5 +1,6 @@
 package com.jorge;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -7,6 +8,25 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.jorge.repository")
 public class CrudSpringApplication {
+
+  static {
+
+    Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+    String dbUrl = dotenv.get("SPRING_DATASOURCE_URL");
+    String dbUsername = dotenv.get("SPRING_DATASOURCE_USERNAME");
+    String dbPassword = dotenv.get("SPRING_DATASOURCE_PASSWORD");
+
+    if (dbUrl != null) {
+      System.setProperty("SPRING_DATASOURCE_URL", dbUrl);
+    }
+    if (dbUsername != null) {
+      System.setProperty("SPRING_DATASOURCE_USERNAME", dbUsername);
+    }
+    if (dbPassword != null) {
+      System.setProperty("SPRING_DATASOURCE_PASSWORD", dbPassword);
+    }
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(CrudSpringApplication.class, args);
